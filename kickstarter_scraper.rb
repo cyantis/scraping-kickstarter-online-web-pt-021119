607b14a7146faca5a20b3b1ba85cddb7629b5e1d
Cody Yantis <cyantis@gmail.com>
2019-03-26 13:44:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,4 +12,5 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css(".location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 end
